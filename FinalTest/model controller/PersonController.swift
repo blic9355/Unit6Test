df0e9eedb5560528e24c84e6c7bf533a08989b88
Brian Licea <brianlicea@Brians-MacBook-Air.local>
2017-10-27 10:56:28 -0600

    first commit

---
+++ FinalTest/model controller/PersonController.swift
@@ -0,0 +1,9 @@
+//
+//  PersonController.swift
+//  FinalTest
+//
+//  Created by Brian Licea on 10/27/17.
+//  Copyright © 2017 Brian Licea. All rights reserved.
+//
+
+import Foundation
---
+++ FinalTest/model/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  FinalTest
+//
+//  Created by Brian Licea on 10/27/17.
+//  Copyright © 2017 Brian Licea. All rights reserved.
+//
+
+import Foundation
