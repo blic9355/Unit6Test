6d33cefe4399281c8ff0a91222cd68203940ed18
Brian Licea <brianlicea@brians-macbook-air.local>
2017-10-27 09:12:08 -0600

    Initial Commit

---
+++ FinalTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalTest
+//
+//  Created by Brian Licea on 10/27/17.
+//  Copyright © 2017 Brian Licea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
